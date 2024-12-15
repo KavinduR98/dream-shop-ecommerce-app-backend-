030aded0e236e5553a51c28065fc0d649fddf974
@kavinduushan07 <kavinduushan07@gmail.com>
2024-12-15 16:56:55 +0530

    create entities and relationships

--- dream-shops/src/main/java/com/ushan/dream_shops/DreamShopsApplication.java
+++ dream-shops/src/main/java/com/ushan/dream_shops/DreamShopsApplication.java
@@ -1,4 +1,4 @@
-package com.ushan.dream_shops;
+ package com.ushan.dream_shops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
