84ad32f7fb43aa523a4dd7bc78c2f2b2d6b0a43c
@kavinduushan07 <kavinduushan07@gmail.com>
2024-12-23 18:45:11 +0530

    add product service and repository layers for managing products

---
+++ dream-shops/src/main/java/com/ushan/dream_shops/exceptions/ProductNotFoundException.java
@@ -0,0 +1,9 @@
+package com.ushan.dream_shops.exceptions;
+
+public class ProductNotFoundException extends RuntimeException{
+
+    public ProductNotFoundException(String message){
+        super(message);
+    }
+
+}
