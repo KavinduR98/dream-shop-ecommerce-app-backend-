b0389db0ad499ee94c84717b4f6451e93e0d682b
@kavinduushan07 <kavinduushan07@gmail.com>
2024-12-25 23:40:49 +0530

    implement Category service with CRUD operations

---
+++ dream-shops/src/main/java/com/ushan/dream_shops/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.ushan.dream_shops.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+
+}
